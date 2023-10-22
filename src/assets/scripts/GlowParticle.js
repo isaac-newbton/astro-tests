7b813af3b97523b50855d649c1b8282435f7b1f4
isaac-newbton <isaac@isaacnewbton.com>
2023-10-22 01:58:29 -0400

    animation

--- src/assets/scripts/GlowParticle.js
+++ src/assets/scripts/GlowParticle.js
@@ -1,17 +1,19 @@
 const PI2 = Math.PI * 2
 
 export class GlowParticle {
-    constructor(x, y, radius, rgb) {
+    constructor(x, y, radius, rgb, lifetime = null) {
         this.x = x
         this.y = y
         this.radius = radius
         this.rgb = rgb
-        this.vx = Math.random() * 20
-        this.vy = Math.random() * 20
+        this.vx = Math.random() * 4
+        this.vy = Math.random() * 4
         this.sinValue = Math.random()
+        this.lifetime = lifetime
+        this.maxLifetime = lifetime
     }
 
-    animate(ctx, stageWidth, stageHeight) {
+    animate(ctx, stageWidth, stageHeight, frameTime = 0) {
         this.sinValue += 0.01
         this.radius += Math.sin(this.sinValue)
         this.x += this.vx
@@ -43,10 +45,20 @@ export class GlowParticle {
             this.y,
             this.radius
         )
-        g.addColorStop(0, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 0.25)`)
+
+        let alphaMax = ((null===this.lifetime) ? 0.833 : 0.333 * ((this.lifetime - frameTime) / this.maxLifetime))
+        if(0 > alphaMax) {
+            alphaMax = 0;
+        }
+
+        g.addColorStop(0, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, ${alphaMax.toString()})`)
         g.addColorStop(1, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 0.0)`)
         ctx.fillStyle = g
         ctx.arc(this.x, this.y, this.radius, 0, PI2, false)
         ctx.fill()
+
+        if(null!==this.lifetime){
+            this.lifetime -= frameTime
+        }
     }
 }
